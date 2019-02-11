88570029c4355ceb03f44b1e35b5ea095002b0b9
Philip James Valentino <pjv@macbooks-MacBook-Pro.local>
2019-02-11 13:01:15 +0000

    topics table migration up & down working

---
+++ db/migrations/20190211123327_create_topics_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex, Promise) {
+  console.log('creating topics table...');
+  return knex.schema.createTable('topics', (topicsTable) => {
+    topicsTable.string('slug').primary();
+    topicsTable.string('description');
+  });
+};
+
+exports.down = function (knex, Promise) {
+  console.log('dropping topics table...');
+  return knex.schema.dropTable('topics');
+};
+
+
+/*
+- Each topic should have:
+  * `slug` field which is a unique string that acts as the table's primary key
+  * `description` field which is a string giving a brief description of a given topic
+*/
