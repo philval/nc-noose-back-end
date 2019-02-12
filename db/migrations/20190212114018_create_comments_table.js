c2d7a02b074b057bce1f77e01c61d0db6d1b8f05
Philip James Valentino <pjv@macbooks-MacBook-Pro.local>
2019-02-12 12:43:04 +0000

    added comments migration and seed

--- db/migrations/20190212114018_create_comments_table.js
+++ db/migrations/20190212114018_create_comments_table.js
@@ -1,12 +1,12 @@
 exports.up = function (knex, Promise) {
   console.log('creating comments table...');
   return knex.schema.createTable('comments', (commentsTable) => {
-    commentsTable.increments('comment_id').primary();
-    commentsTable.string('author').references('users.username');
-    commentsTable.integer('article_id').references('articles.article_id');
-    commentsTable.integer('votes').defaultTo(0);
-    commentsTable.date('created_at').notNullable();
-    commentsTable.text('body').notNullable();
+    commentsTable.increments('comment_id').primary(); // ok
+    commentsTable.string('author').references('users.username'); // map created_by > author
+    commentsTable.integer('article_id').references('articles.article_id'); // map belongs_to > article_id & lookup function
+    commentsTable.integer('votes').defaultTo(0); // ok
+    commentsTable.date('created_at').notNullable(); // date conversion
+    commentsTable.text('body').notNullable(); // ok
   });
 };
 
--- db/seeds/index.js
+++ db/seeds/index.js
@@ -1,4 +1,6 @@
-const { topicData, userData, articleData } = require('../data'); // index.js
+const {
+  topicData, userData, articleData, commentData,
+} = require('../data');
 
 function timestampToDate(timestamp) {
   return new Date(timestamp);
@@ -9,6 +11,22 @@ function formatArticles(articles) {
     article.created_at = timestampToDate(article.created_at);
     return article;
   });
+}
+
+function getArticleIDforComment(comment, returnedArticles) {
+  const foundArticle = returnedArticles.find(article => comment.belongs_to === article.title);
+  return foundArticle.article_id;
+}
+
+// articleRows from Promise returning('*')
+function formatComments(comments, articleRows) {
+  return comments.map(comment => ({ // map makes new object
+    author: comment.created_by, // renamed
+    article_id: getArticleIDforComment(comment, articleRows), // lookup
+    votes: comment.votes, // same
+    created_at: timestampToDate(comment.created_at), // convert date
+    body: comment.body, // same
+  }));
 }
 
 exports.seed = function (knex, Promise) {
@@ -22,6 +40,10 @@ exports.seed = function (knex, Promise) {
     })
     .then(() => {
       const formattedArticles = formatArticles(articleData);
-      return knex('articles').insert(formattedArticles); // explicit return
+      return knex('articles').insert(formattedArticles).returning('*'); // explicit return
+    })
+    .then((articleRows) => {
+      const formattedComments = formatComments(commentData, articleRows);
+      return knex('comments').insert(formattedComments); // explcit return
     });
 };
