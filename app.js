3290984ea9077361134193383695fde8000893e3
philval <526473+gb96@users.noreply.github.com>
2019-03-06 17:26:47 +0000

    added CORS

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const apiRouter = require('./routes/apiRouter.js');
+const cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
 
-const apiRouter = require('./routes/apiRouter.js');
+app.use(cors());
 
 app.use('/api', apiRouter);
 
