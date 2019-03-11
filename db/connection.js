7a460bc286eda27197f6f1368606ce0846e44542
philval <526473+gb96@users.noreply.github.com>
2019-03-11 12:24:08 +0000

    updates to deploy PROD to Heroku

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,8 @@
 const knex = require('knex');
-const dbConfig = require('../knexfile');
+
+const ENV = process.env.NODE_ENV || 'development';
+
+const dbConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile');
 
 const connection = knex(dbConfig);
 module.exports = connection;
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const { PORT = 9090 } = process.env; // default 9090 if not provided...
+const PORT = process.env.PORT || 9090; // default 9090 if not provided...
 
 app.listen(PORT, (err) => {
   if (err) throw (err);
