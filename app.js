c0999f7d1395a7cfedd4debaf95f46bb307c4f5a
philval <526473+gb96@users.noreply.github.com>
2019-03-11 11:36:17 +0000

    change to app.js re CORS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const apiRouter = require('./routes/apiRouter.js');
-const cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
